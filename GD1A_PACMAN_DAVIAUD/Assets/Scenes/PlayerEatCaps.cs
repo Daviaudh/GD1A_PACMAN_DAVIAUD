83060715f407adee8c6f98d4d7d51b5331448d7b
hdaviaud <daviaudh1@gmail.com>
2022-04-11 16:54:46 +0200

    rendus

---
+++ GD1A_PACMAN_DAVIAUD/Assets/Scenes/PlayerEatCaps.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEatCaps : MonoBehaviour
+{
+    public int points = 10;
+
+    private void eat()
+    {
+
+    }
+
+
+
+}
